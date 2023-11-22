2fc2c16c882b732ed06cca3246ab713cd9666019
RayhanHafidh <rayhanhafidh@gmail.com>
2023-11-23 02:29:44 +0700

    fix create petugas

--- resources/views/admin/petugas/create.blade.php
+++ resources/views/admin/petugas/create.blade.php
@@ -14,45 +14,74 @@ Tambah Petugas
           <h6 class="text-s font-bold">Tambah Petugas</h6>
         </div>
         <div class="pb-6 pr-6 pl-6 flex-auto">
-
           <form action="{{ route('admin.petugas.store') }}" method="POST" enctype="multipart/form-data" role="form">
             @csrf
-            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Pilih Cluster</label>
-            <div class="mb-3">
-              <select class="rounded-md text-sm form-control @error('cluster_id') is-invalid @enderror" name="cluster_id"
-                aria-describedby="clusterHelp" id="selectCluster" required>
-                <option value="">-- Pilih --</option>
-                @foreach ($cluster as $cs)
-                <option value="{{ $cs->id }}">{{ $cs->nama }}</option>
-                @endforeach
-              </select>
-              @error('cluster_id')
-              <span id="clusterHelp" class="invalid-feedback" role="alert">
-                <small>{{ $message }}</small>
-              </span>
-              @enderror
-            </div>
             <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Nama petugas</label>
             <div class="mb-4">
               <input type="text"
                 class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
-                id="inputoutlet" placeholder="Masukkan nama Petugas" aria-label="petugas" aria-describedby="petugas-addon"
-                autocomplete="off" value="{{ old('nama')}}" name="nama" required>
-              @error('nama')
-              <span id="petugas-addon" class="invalid-feedback" role="alert"></span>
-
+                id="username" placeholder="Masukkan nama petugas" aria-label="petugas" aria-describedby="petugas-addon"
+                autocomplete="off" value="{{ old('username')}}" name="username" required>
+              @error('username')
+              <span id="username" class="invalid-feedback" role="alert"></span>
               @enderror
             </div>
-            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Alamat</label>
+            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Hak Akses</label>
+            <div class="mb-3">
+              <select class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" name="role"
+                aria-describedby="clusterHelp" id="role" required>
+                <option value="">-- Pilih --</option>
+                <option value="admin">Admin</option>
+                <option value="cabang">Cabang</option>
+                <option value="warehouse">Warehouse</option>
+              </select>
+              @error('role')
+              <span id="role" class="invalid-feedback" role="alert"></span>
+              @enderror
+            </div>
+            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Password</label>
             <div class="mb-4">
-              <textarea
-                class="form-control @error('alamat') is-invalid @enderror flex-auto resize-none focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
-                placeholder="Alamat" aria-label="Alamat" aria-describedby="alamat-addon" name="alamat" id="inputAlamat"
-                rows="3">{{ old('alamat')}}</textarea>
+            <input type="password"
+                class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                id="password" placeholder="Masukkan password" aria-label="password" aria-describedby="password"
+                autocomplete="off" value="{{ old('password')}}" name="password" required>
+              @error('password')
+              <span id="password" class="invalid-feedback" role="alert"></span>
+              @enderror
+            </div>
+            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Masukkan Ulang Password</label>
+            <div class="mb-4">
+            <input type="text"
+                class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                id="password_confirmation" placeholder="Masukkan ulang password" aria-label="password" aria-describedby="password"
+                autocomplete="off" value="{{ old('password_confirmation')}}" name="password_confirmation" required>
+              @error('password_confirmation')
+              <span id="password_confirmation" class="invalid-feedback" role="alert"></span>
+              @enderror
+            </div>
+            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Jenis</label>
+            <div class="mb-4">
+              <input type="text"
+                class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                id="jenis" placeholder="Masukkan nama petugas" aria-label="petugas" aria-describedby="petugas-addon"
+                autocomplete="off" value="{{ old('jenis')}}" name="jenis" required>
+              @error('jenis')
+              <span id="peptugas" class="invalid-feedback" role="alert"></span>
+              @enderror
+            </div>
+            <label class="mb-2 ml-1 font-bold text-xs text-slate-700">Bagian</label>
+            <div class="mb-4">
+              <input type="text"
+                class="flex-auto w-1/4 focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block appearance-none rounded-lg border border-solid border-gray-300 bg-csite bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow"
+                id="bagiane" placeholder="Masukkan nama petugas" aria-label="petugas" aria-describedby="petugas-addon"
+                autocomplete="off" value="{{ old('bagian')}}" name="bagian" required>
+              @error('username')
+              <span id="username" class="invalid-feedback" role="alert"></span>
+              @enderror
             </div>
             <div class="text-center">
-              <button type="submit"
-                class="bg-slate-700 hover:bg-slate-500 inline-block w-1/5 px-6 py-3 mt-6 mb-6 font-bold text-center text-csite uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-fuchsia-700 to-pink-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85">Simpan</button>
+              <button type="submit" class="bg-slate-700 hover:bg-slate-500 text-white inline-block w-1/5 px-6 py-3 mt-6 mb-6 font-bold text-center text-csite uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-xs ease-soft-in tracking-tight-soft bg-gradient-to-tl from-fuchsia-700 to-pink-400 hover:scale-102 hover:shadow-soft-xs active:opacity-85"
+                >Simpan</button>
             </div>
           </form>
 
