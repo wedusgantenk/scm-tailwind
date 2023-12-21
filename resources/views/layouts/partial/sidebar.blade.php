6d5f2588ee6ad46887fae3524d80a29594bafb41
RayhanHafidh <rayhanhafidh@gmail.com>
2023-12-21 14:25:50 +0700

    -barang

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- sidenav  -->
-<aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
+<aside class="max-w-62.5 ease-nav-brand h-full z-990 fixed inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
   <div class="h-19.5">
     <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
     <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="{{ route('dashboard')}}" target="_blank">
@@ -9,7 +9,7 @@
 
   <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
 
-  <div class="items-center block w-auto max-h-screen overflow-y-auto h-sidenav grow basis-full">
+  <div class="items-center block w-auto h-full overflow-y-auto h-sidenav grow basis-full">
     <ul class="flex flex-col pl-0 mb-0">
       <li class="mt-0.5 w-full">
         <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{route('index')}}">
@@ -168,6 +168,14 @@
         <h6 class="pl-6 ml-2 font-bold leading-tight uppercase text-xs opacity-60">Barang</h6>
       </li>
       <li class="mt-0.5 w-full">
+        <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{ route('admin.barang_masuk') }}">
+          <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
+            <i class="fas fa-th-large"></i>
+          </div>
+          <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Barang Masuk</span>
+        </a>
+      </li>
+      <!-- <li class="mt-0.5 w-full">
         <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{ route('admin.barang')}}">
           <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
             <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -186,7 +194,7 @@
           </div>
           <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Barang</span>
         </a>
-      </li>
+      </li> -->
       <li class="mt-0.5 w-full">
         <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{ route('admin.jenis_barang')}}">
           <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
@@ -247,14 +255,6 @@
           <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Histori Barang</span>
         </a>
       </li>
-      <li class="mt-0.5 w-full">
-        <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{ route('admin.barang_masuk') }}">
-          <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
-            <i class="fas fa-th-large"></i>
-          </div>
-          <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Barang Masuk</span>
-        </a>
-      </li>
       <li class="w-full mt-4">
         <h6 class="pl-6 ml-2 font-bold leading-tight uppercase text-xs opacity-60">Distribusi</h6>
       </li>
@@ -274,13 +274,8 @@
           <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Distribusi ke Sales</span>
         </a>
       </li>
-
     </ul>
-
   </div>
-
-
-
 </aside>
 
 
