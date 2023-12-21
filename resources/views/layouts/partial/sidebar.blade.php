cb2184a1212ddddb6d81812f54442387a2795e55
RayhanHafidh <rayhanhafidh@gmail.com>
2023-12-21 15:05:49 +0700

    fix minor bug

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -1,25 +1,23 @@
 <!-- sidenav  -->
-<aside class="max-w-62.5 ease-nav-brand h-full z-990 fixed inset-y-0 my-4 ml-4 block w-full -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
+<aside class="max-w-62.5 ease-nav-brand z-990 fixed inset-y-0 my-4 ml-4 block h-screen w-62.5 -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent">
   <div class="h-19.5">
     <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
     <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="{{ route('dashboard')}}" target="_blank">
       <img src="{{asset('assets/logo.svg')}}" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
     </a>
   </div>
-
   <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent" />
-
   <div class="items-center block w-auto h-full overflow-y-auto h-sidenav grow basis-full">
     <ul class="flex flex-col pl-0 mb-0">
       <li class="mt-0.5 w-full">
         <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors" href="{{route('index')}}">
           <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5">
-            <i class="fas fa-trash-alt fa-sm"></i>
+            <i class="fa fa-tachometer"></i>
           </div>
           <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Dashboard</span>
         </a>
       </li>
-
+      
       <!-- <li class="mt-0.5 w-full">
         <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors hover:bg-pink-500" href="{{ route('admin.cluster') }}">
           <div class="shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center hover:bg-pink-500 stroke-0 text-center xl:p-2.5">
